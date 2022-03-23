861aad5bad4fb57064f6cfdc819ad68e62baaee0
Preet Panchal <77338422+panc0051@users.noreply.github.com>
2022-03-23 13:27:05 -0400

    main

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+//retrieve the HTML  elements
 const $today = document.getElementById('today');
 const $saved = document.getElementById('saved');
-const $form = document.getElementById('form');
+const $formB = document.getElementById('formB');
 const $title = document.getElementById('title');
 const $datetime = document.getElementById('datetime');
 const $userName = document.getElementById('userName');
@@ -25,7 +26,7 @@ $today.textContent = today.toLocaleString('en-CA', options);
 
 //insert today into the $today
 $today.textContent = today.toLocaleString('en-CA', options);
-$form.addEventListener('submit', function (e) {
+$formB.addEventListener('submit', function (e) {
     e.preventDefault()
 
     //get the title
@@ -69,6 +70,9 @@ if (ls) {
 
 // select the elements
 const $list = $('#list')
+const $items =$('.list-group-item')
+const $formAdd = $('#formAdd')
+const $item = $('#item')
 
 //create data array
 const items = [
@@ -97,4 +101,65 @@ function buildList() {
 //build list
 buildList()
 
+//Adding an event handler to the form
+$formAdd.on('submit', function (e) {
+    //prevent the default action
+    e.preventDefault()
 
+    //add new item to items array
+    //new items comes from $item textbox
+    items.push($item.val())
+
+    //reset $item textbox to empty
+    $item.val('')
+
+    //build list
+    buildList()
+
+})
+
+
+
+// use event delegation to set event to existing elements and future elements
+$list.on('click', 'li', function (e) {
+    $(this).toggleClass('text-muted text-decoration-line-through')
+})
+
+//retrieve the HTML elements
+const $body = document.body
+const $title1 = document.getElementById('title1')
+const $form1 = document.getElementById('form1')
+const $bgcolor = document.getElementById('bgcolor')
+const $invert = document.getElementById('invert')
+
+function setBackground() {
+    //to check if a checkbox is checked
+    //.checked = true or false
+    //if inverted is checked
+    if ($invert.checked) {
+        $body.style.color =$bgcolor.value
+        $body.style.backgroundColor = 'rgb(33, 37, 41)'
+
+    } else {
+        //if invert checkbox is not checked
+        //set the baground of the body
+    $body.style.backgroundColor = $bgcolor.value
+    $body.style.color = 'rgb(33, 37, 41)'
+    }
+
+
+}
+
+//listen for changes in the bgcolor
+$bgcolor.addEventListener('change', setBackground)
+
+//Listen for change to the invert checkbox
+$invert.addEventListener('change', setBackground)
+
+
+//form event listener
+//prevent the default action
+$form1.addEventListener('submit', function (e) {
+e.preventDefault()
+
+})
