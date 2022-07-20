6b86a506c61d1d97bc35f6d14fa9d7a0a75af1dd
Preet Panchal <panc0051@algonquinlive.com>
2022-07-20 08:13:23 -0400

    Completes the assignment

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const form = document.querySelector('#form');
+const $form = document.getElementById('form');
+const $formTitle = document.getElementById('form-title');
+const $formType = document.getElementById('form-type');
+const $formLocation = document.getElementById('form-location');
+const $formDescription = document.getElementById('form-description');
+const $formSaved = document.getElementById('form-saved ');
+const $formSubmit = document.getElementById('form-submit');
 
 
+$form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // clear the form
+    $form.reset();
+});
