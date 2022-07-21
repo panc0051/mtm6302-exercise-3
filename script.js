c4ef82ce31edccd4d5fffc21a91f023a4dec5a5d
Preet Panchal <panc0051@algonquinlive.com>
2022-07-20 22:58:35 -0400

    Completes the project

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-// Form Data
-const formData = [
-    'Full Name :',
-    'job Type :',
-    'Job Location :',
-    'job Description :',
-
-]
-
-console.log(formData);
-
-
-// //Retrieve the elements from DOM
-const $form = document.getElementById('form');
-const $formTitle = document.getElementById('form-title');
-const $formType = document.getElementById('form-type');
-const $formLocation = document.getElementById('form-location');
-const $formDescription = document.getElementById('form-description');
-const $formSaved = document.getElementById('form-saved ');
-const $formSubmit = document.getElementById('form-submit');
 const $today = document.getElementById('today');
+const $saved = document.getElementById('saved');
+const $form = document.getElementById('form');
+const $title = document.getElementById('title');
+const $formlabel = document.getElementById('form-label');
+const $datetime = document.getElementById('datetime');
+const $submit = document.getElementById('submit');
 
-// insert todays date
+
+//date constructor
+// create a date instances using new Date()
 const today = new Date();
-$today.textContent = today.toDateString('en-US');
-// // create empty array to hold html
-let formHtml = [];
 
-// 
+const options = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+
+}
+
+$today.textContent = today.toLocaleDateString('en-CA', { options });
 
 $form.addEventListener('submit', function (e) {
     e.preventDefault();
+    //get form values
+    const title = $title.value
 
-    // clear the form
-    $form.reset();
-});
+    //get the date and time
+    const date = new Date($datetime.value);
+
+    // display title and date 
+    $saved.textContent = `${title} : ${date.toLocaleDateString('en-CA', options)}`;
+
+    // create a data object
+    // store datetime as timestamp
+    const data = {
+        form: form,
+        title: title,
+        timestamp: date.getTime()
+
+    }
+
+    // store form data and date in local storage
+    localStorage.setItem('savedDate', JSON.stringify(data));
+
+    // The form should pre-filled with data from local storage when the page first loads.
+    // get the data from local storage
+    const savedDate = localStorage.getItem('savedDate');
+    // parse the data
+    const parsedDate = JSON.parse(savedDate);
+    // display the data
+    $saved.textContent = `${parsedDate.form} : ${parsedDate.title} : ${parsedDate.timestamp}`;
+
+
+
+
+
+
+
+
+})
+
+const ls = localStorage.getItem('savedDate');
+
+// if no save dates, ls equals undefined
+
+if (ls) {
+    const data = JSON.parse(ls);
+    const title = data.title;
+    const datetime = new Date(data.timestamp);
+
+    $saved.textContent = `${data.form} : ${datetime.toLocaleDateString('en-CA', options)}`;
+}
+
+
