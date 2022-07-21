b8cf2addcb2670ce9a404d4e85e7fff17fa36181
Preet Panchal <panc0051@algonquinlive.com>
2022-07-20 21:19:26 -0400

    Completes the assignment

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Form Data
+const formData = [
+    'Full Name :',
+    'job Type :',
+    'Job Location :',
+    'job Description :',
+
+]
+
+console.log(formData);
+
+
+// //Retrieve the elements from DOM
 const $form = document.getElementById('form');
 const $formTitle = document.getElementById('form-title');
 const $formType = document.getElementById('form-type');
@@ -5,7 +18,15 @@ const $formLocation = document.getElementById('form-location');
 const $formDescription = document.getElementById('form-description');
 const $formSaved = document.getElementById('form-saved ');
 const $formSubmit = document.getElementById('form-submit');
+const $today = document.getElementById('today');
 
+// insert todays date
+const today = new Date();
+$today.textContent = today.toDateString('en-US');
+// // create empty array to hold html
+let formHtml = [];
+
+// 
 
 $form.addEventListener('submit', function (e) {
     e.preventDefault();
